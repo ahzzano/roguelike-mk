091f3a236701e490c77b823a03a568e5a3dcf951
W. Santos <w.santos.gg@gmail.com>
2024-07-01 19:48:33 +0800

    player movement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use bevy::{prelude::*, window::PrimaryWindow};
+
+mod board;
+mod player;
+
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (player::spawn_player, spawn_camera))
+        .add_systems(Update, player::move_player)
+        .run();
 }
---
+++ src/player.rs
@@ -0,0 +1,56 @@
+use bevy::{prelude::*, window::PrimaryWindow};
+
+#[derive(Component)]
+pub struct Player {
+    speed: f32,
+}
+
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_server.load("player_scaled.png"),
+            ..default()
+        },
+        Player { speed: 5.0 },
+    ));
+}
+
+pub fn move_player(
+    mut commands: Commands,
+    mut player: Query<&mut Transform, With<Player>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = player.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::KeyW) {
+            direction += Vec3::new(0.0, 1.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::KeyS) {
+            direction += Vec3::new(0.0, -1.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::KeyA) {
+            direction += Vec3::new(-1.0, 0.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::KeyD) {
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        }
+
+        if direction.length() > 1.0 {
+            direction = direction.normalize();
+        }
+
+        transform.translation += direction * 500.0 * time.delta_seconds();
+    }
+}
